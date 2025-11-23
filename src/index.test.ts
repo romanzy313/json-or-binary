9263f4d599a19f61c0bfc749d376f6c5ee87b35d
Roman <romanvolovoy@gmail.com>
2025-11-24 00:26:16 +0100

    stable implementation?

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,86 @@
 import { describe, it, expect } from "vitest";
-import { hello } from "./index";
+import { binarize, parse } from "./index";
 
-describe("smoke", () => {
-  it("tests", () => {
-    const result = hello();
-    expect(result).toEqual("hello world");
+type TestType =
+  | {
+      type: "json";
+      value: string;
+    }
+  | {
+      type: "binary";
+      value: Uint8Array;
+    };
+
+it("encodes and decodes json", () => {
+  const binary = binarize<TestType>({ type: "json", value: "hello, world" });
+
+  expect(binary).toEqual(
+    new TextEncoder().encode(`{"type":"json","value":"hello, world"}`),
+  );
+
+  const parsed = parse<TestType>(binary);
+
+  expect(parsed).toStrictEqual({ type: "json", value: "hello, world" });
+});
+
+it("encodes and decodes binary", () => {
+  const binary = binarize<TestType>({
+    type: "binary",
+    value: new TextEncoder().encode(`42`),
   });
+
+  expect(binary).toEqual(new TextEncoder().encode(`"binary"42`));
+
+  const parsed = parse<TestType>(binary);
+
+  expect(parsed).toStrictEqual({
+    type: "binary",
+    value: new TextEncoder().encode(`42`),
+  });
+});
+
+it("encodes and decodes binary with 0 length", () => {
+  const binary = binarize<TestType>({
+    type: "binary",
+    value: new Uint8Array(),
+  });
+
+  expect(binary).toEqual(new TextEncoder().encode(`"binary"`));
+
+  const parsed = parse<TestType>(binary);
+
+  expect(parsed).toStrictEqual({
+    type: "binary",
+    value: new Uint8Array(),
+  });
+});
+
+it("handles invalid input with quote", () => {
+  // expect this to throw an error
+  expect(() =>
+    binarize({
+      type: 'invalid"',
+      value: "whatever",
+    }),
+  ).toThrowErrorMatchingInlineSnapshot(
+    `[Error: Character '"' is not allowed in type]`,
+  );
+});
+
+it("handles invalid binary data length", () => {
+  // expect this to throw an error
+  expect(() =>
+    parse(new TextEncoder().encode(`"`)),
+  ).toThrowErrorMatchingInlineSnapshot(
+    `[MalformedBinaryDataError: Invalid length]`,
+  );
+});
+
+it("handles malformed binary data without closing type", () => {
+  // expect this to throw an error
+  expect(() =>
+    parse(new TextEncoder().encode(`"whatever is not closed`)),
+  ).toThrowErrorMatchingInlineSnapshot(
+    `[MalformedBinaryDataError: Invalid discriminator]`,
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,66 @@
-export function hello() {
-  return "hello world";
+const binarySplitter = '"' as const;
+const discriminatedName = "type" as const;
+const jsonStartChar = "{".charCodeAt(0);
+const binaryStartChar = binarySplitter.charCodeAt(0);
+
+export type AnyDiscriminatedValue = {
+  [discriminatedName]: string;
+  value: unknown;
+};
+
+export function binarize<T extends AnyDiscriminatedValue>(
+  dValue: T,
+): Uint8Array {
+  if (dValue.type.includes(binarySplitter)) {
+    throw new Error(`Character '${binarySplitter}' is not allowed in type`);
+  }
+
+  // check if value is of binary type like Uint8Array
+
+  if (dValue.value instanceof Uint8Array) {
+    // encode it like ^{type}^{binaryValue}
+    return new Uint8Array([
+      ...new TextEncoder().encode(
+        binarySplitter + dValue.type + binarySplitter,
+      ),
+      ...dValue.value,
+    ]);
+  }
+
+  const stringified = JSON.stringify(dValue);
+
+  return new TextEncoder().encode(stringified);
+}
+
+export function parse<T extends AnyDiscriminatedValue>(data: Uint8Array): T {
+  // minimum payload binary size is '"a"'
+  if (data.byteLength < 3) {
+    throw new MalformedBinaryDataError("Invalid length");
+  }
+
+  const first = data[0]!;
+
+  if (first === jsonStartChar) {
+    // its just json
+    return JSON.parse(new TextDecoder().decode(data)) as T;
+  } else if (first === binaryStartChar) {
+    // read the type until another '"'
+    for (let i = 1; i < data.length; i++) {
+      if (data[i] === binaryStartChar) {
+        const type = new TextDecoder().decode(data.slice(1, i));
+        const value = data.slice(i + 1);
+        return { [discriminatedName]: type, value } as T;
+      }
+    }
+    throw new MalformedBinaryDataError("Invalid discriminator");
+  } else {
+    throw new MalformedBinaryDataError("Invalid starting character");
+  }
+}
+
+class MalformedBinaryDataError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "MalformedBinaryDataError";
+  }
 }
